7352e92b6c01fae497d65cbfe91b166e12271159
Nando2003 <nandofontes27@gmail.com>
2024-07-17 14:24:47 -0300

    refact: the bot class was changed to rpa_2.py file

---
+++ editing_xlsx.py
@@ -0,0 +1,25 @@
+from openpyxl import load_workbook
+from openpyxl.styles import PatternFill
+
+def editing_xlsx(excel_path:str, data:list, linha:int =1, coluna:str ='A') -> bool:
+    """ data = [(data, color), (data,color)...] """
+    try:
+        load_wb = load_workbook(excel_path)
+        load_ws = load_wb.active
+        
+        for i, (value, color) in enumerate(data, start=linha):
+            cell = load_ws[f'{coluna}{i}']
+            cell.value = value
+            if color:
+                cell.fill = PatternFill(
+                    start_color=color,
+                    end_color=color,
+                    fill_type='solid'
+                )
+                
+        load_wb.save(excel_path)
+        return True
+    
+    except Exception:
+        return False
+    
--- import_xlsx.py
+++ import_xlsx.py
@@ -1,10 +1,18 @@
 from openpyxl import load_workbook
+from openpyxl.utils.exceptions import InvalidFileException
 
 def import_column_from_xlsx(excel_path:str, linha:int =1, coluna:str ='A') -> list:
-    load_wb = load_workbook(excel_path)
-    sheet = load_wb.active
-    
-    cell_ID = [cell for cell in sheet[coluna][linha:]]
-    cell_value = [cell.value for cell in cell_ID if cell.value is not None]
+    try:
+        load_wb = load_workbook(excel_path)
+        sheet = load_wb.active
+        
+        cell_ID = [cell for cell in sheet[coluna][linha:]]
+        cell_value = [cell.value for cell in cell_ID if cell.value is not None]
+        
+    except ValueError:
+        raise ValueError("Digite uma coluna/linha existente!")
 
+    except InvalidFileException:
+        raise InvalidFileException("O caminho deverá levar até um arquivo .xlsx")
+    
     return cell_value
